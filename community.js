358f8dce61ed9bf71f46288380a9e67476a40054
subject1486 <bradgoyvaerts@gmail.com>
2015-05-06 13:11:05 +0200

    inital commit

---
+++ community.js
@@ -0,0 +1,14 @@
+    var tmpl = document.querySelector('#tmpl');
+    tmpl.selected = 'Overzicht';
+    tmpl.selectedTab = 'Lijst';
+    tmpl.selectedTabMe = 'mijnIdeeen';
+
+//document.addEventListener('polymer-ready', function(){
+//    
+//    var menuItem = document.getElementById('menu');
+//    var drawer = document.getElementById('drawer');
+//    
+//    menuItem.addEventListener('click', function(){
+//        drawer.togglePanel();
+//    });
+//});
